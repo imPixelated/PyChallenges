3511bb2a7fb618addf64ed0ca754ee0263b19be3
Gustavo Fernandes <120611374+imPixelated@users.noreply.github.com>
2026-04-17 12:16:33 -0300

    Add files via upload

---
+++ Desafio3.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Digite o primeiro número:"))
+numero2 = int(input("Digite o segundo número:"))
+# Se você não especificar que é um número inteiro, ele vai colar os números, ao invés de somar.
+
+soma = numero1 + numero2
+
+print(soma)
